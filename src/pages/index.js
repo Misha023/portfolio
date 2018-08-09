1fc65f7f2c0fc3510869a534b98630579b9c7b34
Misha <mishascholte@gmail.com>
2018-08-09 20:36:04 +0200

    Removed class from contact button

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ const IndexPage = () => (
           <p>I investigate behavioral patterns through user research, design flows with <a href="https://www.overflow.io/" title="Overflow" target="_blank">Overflow </a> and <a href="https://www.sketchapp.com/" title="Sketch" target="_blank"> Sketch </a> and prototype ideas with <a href="https://framer.com/" title="Framer" target="_blank">Framer</a> and <a href="http://principleformac.com/" title="Principle" target="_blank"> Principle</a>. In my spare time I dabble with HTML5 and React to keep up to date with new technologies and possibilities.</p>
           <p>In the past I've worked as Interaction Designer, Product Owner and Marketing Manager. All these different roles have given me a great perspective about what it takes to design a great product. I’ve won and tasted success, but also failed and learned a lot.</p>
           <p>Chances are that if you’re reading this, you are involved in the creation of a modern digital product. If we have that in common, get in touch and let’s grab a coffee.</p>
-          <div className="footer">
+          <div>
               <a href="mailto:hello@mishascholte.nl" title="hello@mishascholte.nl"><button>Contact</button></a>
           </div>
         </div>
