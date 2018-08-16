a49303f68b2d839ec1d056467d5e3cce98d9f1c3
Misha <mishascholte@gmail.com>
2018-08-16 07:51:49 +0200

    Show hero on mobile

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import './Footer.css'
+
+export default () => (
+    <Container>
+      <h1>About CSS Modules</h1>
+      <p>CSS Modules are cool</p>
+    </Container>
+  );
